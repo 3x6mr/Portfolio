1c26229e8027e09f6e1ee1e68460f5008ad1af4a
3x6mr <samr87684@gmail.com>
2025-11-07 19:30:22 +0300

    Home Page UI

--- interview-assistant/app/(root)/layout.tsx
+++ interview-assistant/app/(root)/layout.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
 import React, { ReactNode } from "react";
 
 const Rootlayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="root-layout">
+      <Link href="/" className="flex gap-2 item-center">
+        <Image src="/logo.svg" alt="logo" width={38} height={32} />
+        <h2 className="text-primary-100">PrepWise</h2>
+      </Link>
+      {children}
+    </div>
+  );
 };
 
 export default Rootlayout;
